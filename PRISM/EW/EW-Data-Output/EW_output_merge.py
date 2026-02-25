e7e8c80d9b4d50adab3469cfeed3e81c481ab3e4
nigomezcr <nigomezcr@unal.edu.co>
2026-02-25 01:04:44 -0600

    Complete data output in a single file, generated from individual files by the .py script

---
+++ PRISM/EW/EW-Data-Output/EW_output_merge.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import glob
+import os
+
+
+folder='./'
+files=glob.glob(os.path.join(folder, "*.tsv"))
+
+df_merged=pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
+
+df_merged.to_csv("EW_output_v3.tsv", sep='\t')
+
